1f65fe798b6f8e4db64dc6b1efdf736875e3bdd4
Rethabile <rethabileliphalane213@gmail.com>
2026-06-03 09:22:49 +0000

    Initial Commit

--- index.js
+++ index.js
@@ -2,15 +2,133 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose=require("mongoose")
+const mongodb=require("mongodb")
+console.log("DATABASE:", process.env.DATABASE)
+mongoose.connect(process.env.DATABASE)
+const {Schema}=mongoose
 
+const userSchema=new Schema({
+  username:String
+})
+const exerciseSchema=new Schema({
+  user_id:{type:String,required:true},
+  description:String,
+  duration:Number,
+  date:Date
+})
+
+const Exercise=mongoose.model("Exercises", exerciseSchema)
+const User=mongoose.model("Users", userSchema)
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get("/api/users",async(req,res)=>{
+  const users=await User.find({}).select("_id username")
+  if(!users){
+    res.send("Users not Found")
+    return
+  }else{
+    res.json(users)
+  }
+})
 
+app.get("/api/users/:_id/logs",async(req,res)=>{
+  const {from, to,limit}=req.query
+  const id=req.params._id
+ const user=await User.findById(id)
+  if(!user){
+    res.send("Did not find User")
+    return
+  }
+  let dateObj={}
+  if(from){
+    dateObj["$gte"]=new Date(from)
+  }
+  if(to){
+    dateObj["$lte"]=new Date(to)
+  }
+  let filter={
+    user_id:id
+  }
+  if(from || to){
+    filter.date=dateObj
+  }
+ 
+ const exercises=await Exercise.find(filter).limit(limit ? +limit : 500)
+   const log=exercises.map(e=>({
+    description:e.description,
+    duration: e.duration,
+    date:e.date.toDateString()
+   }))
+  res.json({
+    username:user.username,
+    count:exercises.length,
+    id:user._id,
+    log
+  })
+})
 
+app.post("/api/users",async(req,res)=>{
+console.log(req.body)
+const userObj=new User({
+  username:req.body.username
+})
+try{
+  const user=await userObj.save()
+  res.json(user)
+
+}catch(error){
+console.log(error)
+res.send("Error creating user")
+
+}
+})
+
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const id = req.params._id
+  const { duration, description, date } = req.body
+
+  try {
+    const user = await User.findById(id)
+
+    if (!user) {
+      return res.send("user not found")
+    }
+
+    const exerciseObj = new Exercise({
+      user_id: user._id,
+      description,
+      duration,
+      date: date ? new Date(date) : new Date()
+    })
+
+    try {
+      const exercise = await exerciseObj.save()
+
+      res.json({
+        user_id: user._id,
+        username: user.username,
+        description: exercise.description,
+        duration: exercise.duration,
+        date: new Date(exercise.date).toDateString()
+      })
+
+    } catch (error) {
+      console.log(error)
+      res.send("Error occured while Saving")
+    }
+
+  } catch (error) {
+    console.log(error)
+    res.send("Error occured while Saving")
+  }
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
